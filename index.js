b597e11665fc21eb864b4558e31e862ff72da7e2
Roshik <duwalroseek@gmail.com>
2023-09-03 16:33:40 +0545

    changes

--- index.js
+++ index.js
@@ -7,10 +7,32 @@
 const mobile_nav = document.querySelector('.mobile-navbar-btn');
 const headerELem = document.querySelector(".header");
 
+const nav_link=document.querySelector(".navbar-link");
+const about_link=document.querySelector(".about-link");
+const service_link=document.querySelector(".service-link");
+const contact_link=document.querySelector(".contact-link");
+const client_link=document.querySelector(".client-link");
+
 
 mobile_nav.addEventListener("click",()=>{
     headerELem.classList.toggle('active');
-    console.log("Hello");
+})
+
+nav_link.addEventListener("click",()=>{
+    headerELem.classList.remove('active')
+})
+about_link.addEventListener("click",()=>{
+    headerELem.classList.remove('active')
+})
+service_link.addEventListener("click",()=>{
+    headerELem.classList.remove('active')
+})
+contact_link.addEventListener("click",()=>{
+    headerELem.classList.remove('active')
+})
+
+client_link.addEventListener("click",()=>{
+    headerELem.classList.remove('active')
 })
 
 
@@ -22,8 +44,6 @@ mobile_nav.addEventListener("click",()=>{
 const heroSection= document.querySelector(".section-hero")
 const observer = new IntersectionObserver((entries)=>{
     const ent = entries[0];
-    console.log(ent);
-    console.log(ent.isIntersecting);
     !ent.isIntersecting
     ?document.body.classList.add("sticky")
     :document.body.classList.remove("sticky")
